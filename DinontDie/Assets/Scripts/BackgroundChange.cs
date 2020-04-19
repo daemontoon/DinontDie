3266ee2ee0f41fb2e63dbdf2bcd9fbbdbe923c61
Chiara <chiararosmarino@hotmail.fr>
2020-04-19 14:10:01 +0200

    Trucs pas aboutis

---
+++ DinontDie/Assets/Scripts/BackgroundChange.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChange : MonoBehaviour
+{ }
+    public GameObject BackgroundPrehistoric;
+    public GameObject BackgroundAntiquity;
+    public GameObject BackgroundMedieval;
+    public GameObject BackgroundRenaissance;
+    public GameObject BackgroundModernTimes;
+    public GameObject BackgroundFuture;
+{
+    void Start()
+    {
+        
+
+    BackgroundPrehistoric.gameObject.SetActive(true);
+    BackgroundAntiquity.gameObject.SetActive(false);
+    BackgroundMedieval.gameObject.SetActive(false);
+    BackgroundRenaissance.gameObject.SetActive(false);
+    BackgroundModernTimes.gameObject.SetActive(false);
+    BackgroundFuture.gameObject.SetActive(false);
+
+
+    }
+
+    void Update()
+    
+    {
+
+    yield return new WaitForSeconds(2);
+
+    BackgroundPrehistoric.gameObject.SetActive(false);
+    BackgroundAntiquity.gameObject.SetActive(true);
+
+    yield return new WaitForSeconds(2);
+
+    BackgroundAntiquity.gameObject.SetActive(false);
+    BackgroundMedieval.gameObject.SetActive(true);
+
+    yield return new WaitForSeconds(2);
+
+    BackgroundMedieval.gameObject.SetActive(false);
+    BackgroundRenaissance.gameObject.SetActive(true);
+
+    yield return new WaitForSeconds(2);
+
+    BackgroundRenaissance.gameObject.SetActive(false);
+    BackgroundModernTimes.gameObject.SetActive(true);
+
+    yield return new WaitForSeconds(2);
+
+    BackgroundModernTimes.gameObject.SetActive(false);
+    BackgroundFuture.gameObject.SetActive(false);
+
+    Debug.Log("Bravo tu as gagné mdr");
+    yield return new WaitForSeconds(2);
+}
+
+
+
+}
+}
